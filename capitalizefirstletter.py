7054eabc79d7b77f9c6130da3cd23e7a518f04d6
SAI PHANI SAKETH VARMA <50575738+saketh4567@users.noreply.github.com>
2021-06-25 19:44:00 +0530

    Add files via upload

---
+++ capitalizefirstletter.py
@@ -0,0 +1,17 @@
+a=input()
+b=list(a)
+space=False
+for i in range(len(b)):
+    if i==0:
+        b[i]=b[i].upper()
+    elif b[i]==' ':
+        space=True
+    elif space:
+        space=False
+        b[i]=b[i].upper()
+print(''.join(b))
+"""b=a.split(" ")
+for i in range(len(b)):
+    b[i]=b[i].capitalize()
+print(b)
+print(' '.join(b))"""
